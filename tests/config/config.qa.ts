4551d2c67c42f9c3f86f27a837e00cf0eb7fea91
Ketan Dhumal <ketan.dhumaltechholding.co@Ketans-MacBook-Pro-2.local>
2024-12-09 16:17:57 +0530

    Resolved Conflict

--- tests/config/config.qa.ts
+++ tests/config/config.qa.ts
@@ -7,8 +7,8 @@ export const config = {
   errorMessage: "Incorrect email or password. Please try again.",
   inviteEmail: 'test@gmail.com',
   expectedSuccessMessage: "Invitation sent successfully!",
-  expectedroleErrorMessage:'Role is required',
-  expecteddepartmentErrorMessage:'Department is required',
-  expectedgroupsErrorMessage:'At least one group must be selected',
+  expectedroleErrorMessage: 'Role is required',
+  expecteddepartmentErrorMessage: 'Department is required',
+  expectedgroupsErrorMessage: 'At least one group must be selected',
 };
 
