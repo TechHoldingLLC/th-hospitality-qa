5975b9b2dcba1612550d62ee1e394945a78ee9dd
karishma-barot <karishma.barot@techholding.co>
2025-01-10 11:17:37 +0530

    Drafted code for event creation

--- tests/pageObjects/endToEndFlow.ts
+++ tests/pageObjects/endToEndFlow.ts
@@ -1,9 +1,68 @@
 import { Page } from "@playwright/test";
 import BasePage from "./basePage";
+import { adminCreateEditPackagePage } from "./adminCreateEditPackage";
+import path from "path";
+let eventCreationWizard: adminEventCreationWizard;
+let createEditPackagePage: adminCreateEditPackagePage;
 
-export class endToEndFlow extends BasePage{
+export class endToEndFlow extends BasePage {
+  constructor(page: Page) {
+    super(page);
+    eventCreationWizard = new eventCreationWizard(page);
+    createEditPackagePage = new adminCreateEditPackagePage(page);
+  }
 
-    constructor(page: Page){
-        super(page);
-    }
+  async createEventWithNewObjects(eventname: string) {
+    //await this.addDataToEventInformationForm(eventname);
+    await this.clickElement(eventCreationWizard.addProductButton);
+    await createEditPackagePage.createProductUnderPackage(
+      await this.generateNomenclatureName("Product_Under_Event")
+    );
+    await this.waitForElementHidden(eventCreationWizard.productNameInput);
+    await this.clickElement(eventCreationWizard.nextButton);
+    await this.clickElement(eventCreationWizard.addPackageButton);
+    await eventCreationWizard.createPackageUnderEvent();
+    await this.clickElement(eventCreationWizard.publishButton);
+  }
+
+  async addDataToEventInformationForm(eventname: string, programname: string) {
+    await this.enterValuesInElement(
+      eventCreationWizard.eventNameInput,
+      eventname
+    );
+
+    // Generate start date & end date
+    const startDate = await this.getRandomFutureDate();
+    const endDate = new Date(startDate);
+    endDate.setDate(endDate.getDate() + 5); // Add 5 days to the start date
+    const endDateFormatted = `${String(endDate.getMonth() + 1).padStart(
+      2,
+      "0"
+    )}-${String(endDate.getDate()).padStart(2, "0")}-${endDate.getFullYear()}`;
+
+    await this.clickElement(eventCreationWizard.eventStartDateInput);
+    await this.page.keyboard.type(startDate);
+    await this.clickElement(eventCreationWizard.eventEndDateInput);
+    await this.page.keyboard.type(endDateFormatted);
+
+    await this.page.waitForTimeout(3000);
+    await this.clickOnRandomOptionFromDropdown(
+      eventCreationWizard.eventVenueDropdown
+    );
+    await this.page.waitForTimeout(3000);
+    await eventCreationWizard.associatedProgramDropdown.selectOption(
+      programname
+    );
+
+    await eventCreationWizard.thumbnailUploadInput.setInputFiles(
+      path.join(
+        __dirname,
+        "../data/coca-cola-images/event/Coachella-2020-1280x720-988x416.jpg"
+      )
+    );
+    await eventCreationWizard.mediaUploadInput.setInputFiles(
+      path.join(__dirname, "../data/coca-cola-images/event/images.jpeg")
+    );
+    await this.clickElement(eventCreationWizard.nextButton);
+  }
 }
--- tests/specs/endToEndFlow.spec.ts
+++ tests/specs/endToEndFlow.spec.ts
@@ -11,30 +11,45 @@ let basePage: BasePage;
 let loginPage: adminLoginPage;
 let createProgram: adminCreateEditProgramPage;
 let viewProgram: adminViewProgramsPage;
+//let eventCreationWizardPage: adminEventCreationWizard;
 
-test.beforeEach(async()=>{
-    browser = await chromium.launch({headless: false, channel: "chrome"});
-    page = await browser.newPage();
-    basePage = new BasePage(page);
-    loginPage = new adminLoginPage(page);
-    createProgram = new adminCreateEditProgramPage(page);
-    viewProgram = new adminViewProgramsPage(page);
-    // await basePage.navigateTo(config.adminPortalUrl);
-    // await loginPage.login(config.email, config.password);
-    await basePage.navigateTo("https://admin.dev.hospitality.thinfra.net/login");
-    await loginPage.login("superuser@dev.hospitality.thinfra.net", "TestSuperUser%123");
+test.beforeEach(async () => {
+  browser = await chromium.launch({ headless: false, channel: "chrome" });
+  page = await browser.newPage();
+  basePage = new BasePage(page);
+  loginPage = new adminLoginPage(page);
+  createProgram = new adminCreateEditProgramPage(page);
+  viewProgram = new adminViewProgramsPage(page);
+  // await basePage.navigateTo(config.adminPortalUrl);
+  // await loginPage.login(config.email, config.password);
+  await basePage.navigateTo("https://admin.dev.hospitality.thinfra.net/login");
+  await loginPage.login(
+    "superuser@dev.hospitality.thinfra.net",
+    "TestSuperUser%123"
+  );
 });
 
-test.afterEach(async()=>{
-    await browser.close();
+test.afterEach(async () => {
+  await browser.close();
 });
 
-test("End to end flow", async()=>{
-    const programName = await basePage.generateNomenclatureName('Program');
+test("End to end flow", async () => {
+  try {
+    const programName = await basePage.generateNomenclatureName("Program");
     await basePage.clickElement(viewProgram.programsButton);
     await basePage.clickElement(viewProgram.addProgramButton);
     await createProgram.createProgram(programName);
     expect(
-        await basePage.isElementVisible(createProgram.createSuccessMessage)
-      ).toBe(true);
+      await basePage.isElementVisible(createProgram.createSuccessMessage)
+    ).toBe(true);
+
+    expect(
+      await basePage.isElementVisible(
+        eventCreationWizardPage.eventPublishSuccessMessage
+      )
+    ).toBe(true);
+  } catch (error: any) {
+    console.error(`Test failed: ${error.message}`);
+    throw error;
+  }
 });
