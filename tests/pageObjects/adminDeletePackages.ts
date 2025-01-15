d57dfb34ef0e929484776d782730e5077d882543
Ruturaj8008 <ruturaj.darekar@techholding.co>
2025-01-15 19:00:11 +0530

    COC-245: Add new test cases

--- tests/pageObjects/adminDeletePackages.ts
+++ tests/pageObjects/adminDeletePackages.ts
@@ -1,150 +1,109 @@
-import { expect,Locator, Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import BasePage from "./basePage";
-import deletePackagesData from "../data/deletePackagesData.json";
 
-export class adminDeletePackagesPage extends BasePage{
+export class adminDeletePackagesPage extends BasePage {
+  public menuButton: Locator;
+  public deleteButton: Locator;
+  public alertDialog: Locator;
+  public orderButton: Locator;
+  public orderListLink: Locator;
+  public packagesButton: Locator;
+  public packageNameFromTable: Locator;
+  public nextButton: Locator;
+  public deleteMessageLabel: Locator;
+  public packageDeleteSuccessLabel: Locator;
+  public confirmationButton: Locator;
+  public packageInputText: Locator;
+  public cancelPackageButton: Locator;
+  public deletePackageButton: Locator;
+  public failedToDeletePackageLabel: Locator;
 
-    public threeDotsButton: Locator;
-    public deleteButton:Locator;
-    public alertDialog:Locator;
-    public deleteMessageLabel:Locator;
-    public deletePackageButton:Locator;
-    public conformationButton:Locator;
-    public noResultsLabel:Locator;
+  constructor(page: Page) {
+    super(page);
+    this.menuButton = page.locator('td div button[aria-haspopup="menu"]');
+    this.deleteButton = page.getByRole("menuitem", { name: "Delete" });
+    this.alertDialog = page.getByRole("alertdialog");
+    this.orderButton = page.locator("//a[@href='/orders']");
+    this.orderListLink = page.locator("//table//tr/td[1]//a");
+    this.packagesButton = page.locator(
+      "//div[@role='tablist']/button[text()='Packages']"
+    );
+    this.packageNameFromTable = page.locator(
+      "//div[contains(@id,'package')]//table//tr/td[1]"
+    );
+    this.nextButton = page.locator("//button[text()='Next']");
+    this.deleteMessageLabel = page.locator(
+      "//p[contains(@class,'text-ui-fg-subtle')]/label[1]"
+    );
+    this.deletePackageButton = page.getByRole("button", { name: "Delete" });
+    this.packageDeleteSuccessLabel = page.locator("label", {
+      hasText: "Package deleted",
+    });
+    this.confirmationButton = page.getByRole("button", {
+      name: "Yeah, Thanks!",
+    });
+    this.packageInputText = page.locator("//td//a//span[text()]");
+    this.cancelPackageButton = page.getByRole("button", { name: "Cancel" });
+    this.deletePackageButton = page.getByRole("button", { name: "Delete" });
+    this.failedToDeletePackageLabel = page.getByText(
+      "Failed to delete package"
+    );
+  }
 
-    // order page Locator
-    public orderButton:Locator;
-    public orderListLink:Locator;
-    public packagesButton:Locator;
-    public packageNameFromTable:Locator;
-    public gotItButton:Locator;
-    public nextButton:Locator;
+  async menuButtonByPackageName(packageName: string): Promise<Locator> {
+    return this.page.locator(
+      `//tr[td[1][normalize-space() = '${packageName}']]//button[@aria-haspopup="menu"]`
+    );
+  }
 
-    constructor(page: Page) {
-        super(page);
-        this.threeDotsButton = page.locator("(//a//button[@type='button' and @data-state='closed'])[1]");
-        this.deleteButton = page.locator("//*[@role='menuitem']//span[text()='Delete']");
-        this.alertDialog = page.locator("//div[@role='alertdialog']");
-        this.deleteMessageLabel = page.locator("//p[contains(@class,'text-ui-fg-subtle')]/label[1]");
-        this.deletePackageButton = page.locator("//button[text()='Delete']"); 
-        this.conformationButton  = page.locator("//button[text()='Yeah, Thanks!']"); 
-        this.noResultsLabel = page.locator("text='No results'");
-        this.orderButton = page.locator("//a[@href='/orders']");
-        this.orderListLink = page.locator("//table//tr/td[1]//a");
-        this.packagesButton = page.locator("//div[@role='tablist']/button[text()='Packages']");
-        this.packageNameFromTable = page.locator("//div[contains(@id,'package')]//table//tr/td[1]");
-        this.gotItButton = page.locator("//button[text()='Got it']");
-        this.nextButton = page.locator("//button[text()='Next']");
-
+  async openDeletePopup(packageName?: string): Promise<void> {
+    if (packageName) {
+      await this.clickElement(await this.menuButtonByPackageName(packageName));
+    } else {
+      await this.clickElement(this.menuButton.first());
     }
+    await this.clickElement(this.deleteButton);
+    await this.waitForElementVisible(this.deletePackageButton);
+  }
 
-    // Delete package 
-    async deletePackage(packageName:string){
+  async getPackageNameFromOrderPage(): Promise<string | null> {
+    await this.waitForPageToBeReady();
+    await this.clickElement(this.orderButton);
+    await this.clickElement(this.orderListLink.last());
+    await this.clickElement(this.packagesButton);
+    await this.waitForElementVisible(this.packageNameFromTable.first());
+    await this.clickElement(this.packageNameFromTable.first());
+    return (
+      await this.getAllTextContents(this.packageNameFromTable.first())
+    ).join(" ");
+  }
 
-        // Click on three dot button
-        await this.page.locator('//div[normalize-space()="'+packageName+'"]/ancestor::td/following-sibling::td//button').click();
+  async clickPackageMenuButton(packageName: string): Promise<void> {
+    const packageLocator = this.page.locator(
+      `//div[normalize-space()='${packageName}']/ancestor::td/following-sibling::td//button`
+    );
+    await this.page.waitForTimeout(2000);
+    let packageFound = await packageLocator.isVisible();
 
-        // Click on delete button
-        await this.clickElement(this.deleteButton);
-
-        // Verify alert dialog open or not and verify alert message also
-        expect(await this.isElementVisible(this.alertDialog)).toBe(true);
-        expect(await this.deleteMessageLabel.textContent()).toBe('This will permanently delete the package "'+packageName+'" and cannot be undone.');
-
-        // Click on delete package button
-        await this.clickElement(this.deletePackageButton);
-
-        // Verify delete success message
-        await this.isElementVisible(this.conformationButton);
-        expect(await this.deleteMessageLabel.textContent()).toBe(packageName+' was successfully deleted.');
-
-        // Click on conformation button
-        await this.clickElement(this.conformationButton);
-
-        // Verify popup close or not
-        expect(await this.alertDialog.isHidden()).toBe(false);
+    if (!packageFound) {
+      let isNextEnabled = await this.nextButton.isEnabled();
+      if (isNextEnabled) {
+        await this.clickElement(this.nextButton);
+        await this.waitForPageToBeReady();
+        await this.clickPackageMenuButton(packageName);
+      } else {
+        console.error(`${packageName} not found on the page`);
+      }
+    } else {
+      await this.clickElement(packageLocator);
+      await this.waitForPageToBeReady();
     }
+  }
 
-    // Get Package name from Order page
-    async getPackageNameFromOrderPage():Promise<null | string>{
-
-        // Naviage to Orders page
-        await this.clickElement(this.orderButton);
-
-        // Naviate to Order Details page
-        await this.selectRandomItemFromMultiSelectList(this.orderListLink);
-
-        // Click on Package button
-        await this.clickElement(this.packagesButton);
-
-        await this.page.waitForTimeout(3000);
-
-        // Get text from package list 
-        await this.waitForElementVisible(this.packageNameFromTable.first()); 
-        return await this.packageNameFromTable.first().textContent();
-    }
-
-    // Find package and click on three dot button
-    async clickOnThreeButton(packageName:string){
-
-        const packageLocator: Locator = this.page.locator('//div[normalize-space()="'+packageName+'"]/ancestor::td/following-sibling::td//button');
-
-        // check package is display in page or not
-        await this.page.waitForTimeout(3000);
-        let productFound:boolean = (await packageLocator.isVisible()).valueOf();
-
-        if(!productFound){
-              let isnextButtonEnabled:boolean = (await this.nextButton.isEnabled()).valueOf();
-
-              if(isnextButtonEnabled){
-                // click on next button
-                await this.clickElement(this.nextButton); 
-
-                // wait for page load
-                 await this.page.waitForLoadState('domcontentloaded');
-
-                  await this.clickOnThreeButton(packageName);
-            } else{
-                console.log(packageName+" is not found in this page")
-                expect(false).toBeTruthy();
-            }
-        } else {
-
-            // click on product link
-            await this.clickElement(packageLocator);
-            await this.page.waitForLoadState('networkidle');
-
-            // verifty porperly navigate or not
-            expect(await this.deleteButton.isVisible()).toBe(true);
-        }
-
-    }
-
-    // Delete Assoicated Package and verify warning message
-    async deleteAssociatedPackage(packageName:string){
-
-         // Click on delete button
-         await this.clickElement(this.deleteButton);
-
-         // Verify alert dialog open or not and verify alert message also
-         expect(await this.isElementVisible(this.alertDialog)).toBe(true);
-         expect(await this.deleteMessageLabel.textContent()).toBe('This will permanently delete the package "'+packageName+'" and cannot be undone.');
- 
-         // Click on delete package button
-         await this.clickElement(this.deletePackageButton); 
-         
-         await this.page.waitForTimeout(2000);
-
-         // Verify delete success message
-         await this.isElementVisible(this.gotItButton);
-         expect(await this.deleteMessageLabel.textContent()).toBe(deletePackagesData.AssociatedPackageNotDeleteMessage);
-        
-         // Click on conformation button
-         await this.clickElement(this.gotItButton);
-         
-         // Verify popup close or not
-         expect(await this.waitForElementHidden(this.gotItButton)).not.toBe(false);
-    }
-
+  async attemptToDeletePackage(packageName: string): Promise<void> {
+    await this.clickPackageMenuButton(packageName);
+    await this.clickElement(this.deleteButton);
+    await this.clickElement(this.deletePackageButton);
+    await this.waitForElementVisible(this.failedToDeletePackageLabel);
+  }
 }
-  
--- tests/specs/adminDeletePackages.spec.ts
+++ tests/specs/adminDeletePackages.spec.ts
@@ -1,14 +1,17 @@
 import test, { Browser, Page, chromium, expect } from "@playwright/test";
 import BasePage from "../pageObjects/basePage";
 import { adminLoginPage } from "../pageObjects/adminLoginPage";
+import { adminCreateEditPackagePage } from "../pageObjects/adminCreateEditPackage";
 import { config } from "../config/config.qa";
 import { adminDeletePackagesPage } from "../pageObjects/adminDeletePackages";
 import { adminViewPackagesPage } from "../pageObjects/adminViewPackages";
+import deletePackageData from "../data/deletePackagesData.json";
 
 let browser: Browser;
 let page: Page;
 let basePage: BasePage;
 let loginPage: adminLoginPage;
+let createEditPackagePage: adminCreateEditPackagePage;
 let viewPackagesPage: adminViewPackagesPage;
 let deletePackagesPage: adminDeletePackagesPage;
 
@@ -17,72 +20,118 @@ test.beforeEach(async () => {
   page = await browser.newPage();
   basePage = new BasePage(page);
   loginPage = new adminLoginPage(page);
+  createEditPackagePage = new adminCreateEditPackagePage(page);
   viewPackagesPage = new adminViewPackagesPage(page);
   deletePackagesPage = new adminDeletePackagesPage(page);
   await basePage.navigateTo(config.adminPortalUrl);
   await loginPage.login(config.email, config.password);
-  await basePage.clickElement(viewPackagesPage.packagesButton);
 });
 
 test.afterEach(async () => {
   await browser.close();
 });
 
-test('TC0089 - verify that the user can access a CTA to delete', async() =>{
-    try {
-        // Click on three dot button
-        await basePage.clickElement(deletePackagesPage.threeDotsButton);
+test("TC0089 - Verify that an admin can successfully delete a package that is not associated with any event or program.", async () => {
+  try {
+    // create and validate package.
+    const packageName = await basePage.generateNomenclatureName("Package");
+    await createEditPackagePage.addPackage(packageName);
+    expect(
+      await basePage.isElementVisible(
+        createEditPackagePage.packageCreationSuccessMessage
+      )
+    ).toBe(true);
+    await basePage.waitForElementVisible(
+      await deletePackagesPage.menuButtonByPackageName(packageName)
+    );
+    expect(
+      await basePage.getElementText(deletePackagesPage.packageInputText.first())
+    ).toEqual(packageName);
 
-        // Verify delete button is visible
-        expect(await basePage.isElementVisible(viewPackagesPage.addpackageButton)).toBe(true);
-        
-    } catch (error: any) {
-        console.log(`Test failed: ${error.message}`);
-        throw error;
-    }
+    // Validate delete confirmation message.
+    await deletePackagesPage.openDeletePopup(packageName);
+    expect(
+      await basePage.getElementText(deletePackagesPage.deleteMessageLabel)
+    ).toEqual(
+      `${deletePackageData.expectedDeleteConfirmationMessage} "${packageName}" ${deletePackageData.deleteConfirmationSuffix}`
+    );
 
+    // Delete and validate success message.
+    await basePage.clickElement(deletePackagesPage.deletePackageButton);
+    await basePage.waitForElementVisible(
+      deletePackagesPage.packageDeleteSuccessLabel
+    );
+    expect(
+      await basePage.getElementText(
+        deletePackagesPage.deleteMessageLabel.last()
+      )
+    ).toEqual(
+      `${packageName} ${deletePackageData.expectedDeleteSuccessMessage}`
+    );
+
+    // Validate package delete successfully.
+    await basePage.clickElement(deletePackagesPage.confirmationButton);
+    await basePage.waitForElementHidden(
+      await deletePackagesPage.menuButtonByPackageName(packageName)
+    );
+    expect(
+      await basePage.getElementText(deletePackagesPage.packageInputText.first())
+    ).not.toEqual(packageName);
+  } catch (error: any) {
+    console.log(`Test failed: ${error.message}`);
+    throw error;
+  }
 });
 
-test('TC0090 - Verify that if the package is not associated with orders a confirmation prompt appears', async() =>{
-    try {
-        
-        // Delete Package
-        deletePackagesPage.deletePackage("");
-        
-    } catch (error: any) {
-        console.log(`Test failed: ${error.message}`);
-        throw error;
-    }
+test("TC0090 - Verify that if the package is not associated with orders a confirmation prompt appears", async () => {
+  try {
+    await basePage.clickElement(createEditPackagePage.packagesButton);
+    const verifyAlertDialogVisibleAndHidden = async () => {
+      expect(
+        await basePage.isElementVisible(deletePackagesPage.alertDialog)
+      ).toBe(true);
+      expect(
+        await basePage.getElementText(deletePackagesPage.deleteMessageLabel)
+      ).toContain(deletePackageData.expectedDeleteConfirmationMessage);
+      await basePage.clickElement(deletePackagesPage.cancelPackageButton);
+      await basePage.waitForElementHidden(deletePackagesPage.alertDialog);
+      expect(await deletePackagesPage.alertDialog.isHidden()).toBe(true);
+    };
 
+    // Open delete popup and verify alert dialog behavior
+    await deletePackagesPage.openDeletePopup();
+    await verifyAlertDialogVisibleAndHidden();
+
+    // Open delete popup again and verify alert dialog behavior
+    await deletePackagesPage.openDeletePopup();
+    await verifyAlertDialogVisibleAndHidden();
+  } catch (error: any) {
+    console.error(`Test failed: ${error.message}`);
+    throw error;
+  }
 });
 
-test('TC0091 - Verify that if the package is associated with orders an error message appears', async() =>{
-    try {
-        // Get Package name from Order Page
-        const packageName : null|string= await deletePackagesPage.getPackageNameFromOrderPage();
+test("TC0091 - Verify that if the package is associated with orders an error message appears", async () => {
+  try {
+    const packageName = await deletePackagesPage.getPackageNameFromOrderPage();
+    await basePage.clickElement(viewPackagesPage.packagesButton);
+    await deletePackagesPage.attemptToDeletePackage(packageName!);
 
-        if (packageName === null) {
-            console.error(`Test failed: Null value found while retrieving the text of the package`);
-            expect( packageName,'Null value found while retrieving the text of the package.').not.toBeNull();
-          }  else{
+    expect(
+      await basePage.isElementVisible(
+        deletePackagesPage.failedToDeletePackageLabel
+      )
+    ).toBe(true);
 
-            console.log("Package Name: "+packageName);
+    expect(
+      await basePage.getElementText(deletePackagesPage.alertDialog)
+    ).not.toBeNull();
 
-            // Navigate to Packages page
-            await basePage.clickElement(viewPackagesPage.packagesButton);
-
-            // Click on Three dot button
-            await deletePackagesPage.clickOnThreeButton(packageName);
-
-            // Click on delete button and verify user can not delete package
-            await deletePackagesPage.deleteAssociatedPackage(packageName);
-
-          }
-      
-        
-    } catch (error: any) {
-        console.log(`Test failed: ${error.message}`);
-        throw error;
-    }
-
+    expect(
+      await basePage.getAllTextContents(deletePackagesPage.alertDialog)
+    ).toContain(deletePackageData.failedMessage);
+  } catch (error: any) {
+    console.error(`Test failed: ${error.message}`);
+    throw error;
+  }
 });
