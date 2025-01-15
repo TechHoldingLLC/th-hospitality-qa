4407bd12dbaf14f0cf9e8ee19632a30da5b25b47
Ruturaj8008 <ruturaj.darekar@techholding.co>
2025-01-15 17:41:33 +0530

    COC-243: Add new test cases

--- tests/pageObjects/adminDeleteEvents.ts
+++ tests/pageObjects/adminDeleteEvents.ts
@@ -1,10 +1,63 @@
 import { Locator, Page } from "@playwright/test";
 import BasePage from "./basePage";
 
-export class adminDeleteEventsPage extends BasePage{
+export class adminDeleteEventPage extends BasePage {
+  public searchInput: Locator;
+  public eventInputText: Locator;
+  public menuButtons: Locator;
+  public deleteButton: Locator;
+  public deleteEventButton: Locator;
+  public deleteMessageLabel: Locator;
+  public eventDeleteSuccessLabel: Locator;
+  public confirmationButton: Locator;
+  public noResultsMessageContainer: Locator;
+  public alertDialog: Locator;
+  public cancelEventButton: Locator;
+  public menuButtonWithLocationGreaterThanZero: Locator;
+  public failedToDeleteEventLabel: Locator;
 
-    constructor(page: Page) {
-        super(page);
-    }
+  constructor(page: Page) {
+    super(page);
+    this.searchInput = page.locator('input[placeholder="Search"]');
+    this.eventInputText = page.locator("//td//span[text()]");
+    this.menuButtons = page.locator('td div button[aria-haspopup="menu"]');
+    this.deleteButton = page.getByRole("menuitem", { name: "Delete" });
+    this.deleteEventButton = page.getByRole("button", { name: "Delete" });
+    this.deleteMessageLabel = page.locator(
+      "//p[contains(@class,'text-ui-fg-subtle')]/label[1]"
+    );
+    this.eventDeleteSuccessLabel = page.locator("label", {
+      hasText: "Event deleted",
+    });
+    this.confirmationButton = page.getByRole("button", {
+      name: "Yeah, Thanks!",
+    });
+    this.noResultsMessageContainer = page.locator(
+      ".flex.flex-col.items-center.gap-y-2"
+    );
+    this.alertDialog = page.getByRole("alertdialog");
+    this.cancelEventButton = page.getByRole("button", { name: "Cancel" });
+    this.failedToDeleteEventLabel = page.getByText("Failed to delete product");
+    this.menuButtonWithLocationGreaterThanZero = page.locator(
+      '//tr[td[6] > 0]//button[@aria-haspopup="menu"]'
+    );
+  }
 
+  async menuButtonByEventName(eventName: string): Promise<Locator> {
+    return this.page.locator(
+      `//tr[td[normalize-space() = '${eventName}']]//button[@aria-haspopup="menu"]`
+    );
+  }
+
+  async openDeletePopup(): Promise<void> {
+    await this.clickElement(this.menuButtons.first());
+    await this.clickElement(this.deleteButton);
+    await this.waitForElementVisible(this.deleteEventButton);
+  }
+
+  async deleteReferredEvent(): Promise<void> {
+    await this.clickElement(this.menuButtonWithLocationGreaterThanZero.first());
+    await this.clickElement(this.deleteButton);
+    await this.clickElement(this.deleteEventButton);
+  }
 }
--- tests/specs/adminDeleteEvents.spec.ts
+++ tests/specs/adminDeleteEvents.spec.ts
@@ -1,42 +1,126 @@
-import { test, Browser, Page, chromium, expect } from "@playwright/test";
+import test, { Browser, Page, chromium, expect } from "@playwright/test";
 import BasePage from "../pageObjects/basePage";
+import { adminEventCreationWizardpage } from "../pageObjects/adminEventCreationWizard";
 import { config } from "../config/config.qa";
 import { adminLoginPage } from "../pageObjects/adminLoginPage";
-import { adminViewEventsPage } from "../pageObjects/adminViewEvents";
+import deleteEventData from "../data/deleteEventData.json";
+import { adminDeleteEventPage } from "../pageObjects/adminDeleteEvents";
 
 let browser: Browser;
 let page: Page;
 let basePage: BasePage;
 let loginPage: adminLoginPage;
-let viewEventsPage: adminViewEventsPage;
+let eventCreationWizardPage: adminEventCreationWizardpage;
+let deleteEventPage: adminDeleteEventPage;
 
 test.beforeEach(async () => {
   browser = await chromium.launch({ headless: false, channel: "chrome" });
   page = await browser.newPage();
-  await page.setViewportSize({ width: 1792, height: 1080 });
   basePage = new BasePage(page);
   loginPage = new adminLoginPage(page);
-  viewEventsPage = new adminViewEventsPage(page);
-  //Navigation to admin portal
+  eventCreationWizardPage = new adminEventCreationWizardpage(page);
+  deleteEventPage = new adminDeleteEventPage(page);
   await basePage.navigateTo(config.adminPortalUrl);
-  //Login
   await loginPage.login(config.email, config.password);
-  //Navigation to evets Listing page
-  await basePage.clickElement(viewEventsPage.eventsButton);
+  await basePage.clickElement(eventCreationWizardPage.eventsButton);
 });
 
 test.afterEach(async () => {
   await browser.close();
 });
 
-test('TC0083 - Verify that the user can access a CTA to delete', async()=>{
-    try {
-        
-        
-    } catch (error: any) {
-        console.error(`Test failed: ${error.message}`);
-        throw error;
-    }
+test("TC0083 - Verify that an admin can successfully delete an event that is not associated with programs, products, Packages.", async () => {
+  try {
+    // Create Event
+    const eventName = await basePage.generateNomenclatureName("Event");
+    await basePage.clickElement(eventCreationWizardPage.addEventButton);
+    await eventCreationWizardPage.createEvent(eventName);
+    expect(
+      await basePage.isElementVisible(
+        eventCreationWizardPage.eventDraftSuccessMessage
+      )
+    ).toBe(true);
 
+    // search and validate created event.
+    await basePage.enterValuesInElement(deleteEventPage.searchInput, eventName);
+    await basePage.waitForElementVisible(
+      await deleteEventPage.menuButtonByEventName(eventName)
+    );
+    expect(
+      await basePage.getElementText(deleteEventPage.eventInputText.first())
+    ).toEqual(eventName);
+
+    // Validate delete event confirmation.
+    await deleteEventPage.openDeletePopup();
+    expect(
+      await basePage.getElementText(deleteEventPage.deleteMessageLabel)
+    ).toEqual(
+      `${deleteEventData.expectedDeleteConfirmationMessage} "${eventName}" ${deleteEventData.deleteConfirmationSuffix}`
+    );
+
+    // Validate event delete success message.
+    await basePage.clickElement(deleteEventPage.deleteEventButton);
+    await basePage.waitForElementVisible(
+      deleteEventPage.eventDeleteSuccessLabel
+    );
+    expect(
+      await basePage.getElementText(deleteEventPage.deleteMessageLabel.last())
+    ).toEqual(`${eventName} ${deleteEventData.expectedDeleteSuccessMessage}`);
+
+    // Validate and delete event successfully.
+    await basePage.clickElement(deleteEventPage.confirmationButton);
+    expect(
+      await basePage.getElementText(deleteEventPage.noResultsMessageContainer)
+    ).toContain(deleteEventData.expectedErrorMessages);
+  } catch (error: any) {
+    console.error(`Test failed: ${error.message}`);
+    throw error;
+  }
 });
 
+test("TC0084 - Verify that if the events is not associated with packages, products and orders a confirmation prompt appears.", async () => {
+  try {
+    const verifyAlertDialogVisibleAndHidden = async () => {
+      expect(await basePage.isElementVisible(deleteEventPage.alertDialog)).toBe(
+        true
+      );
+      expect(
+        await basePage.getElementText(deleteEventPage.deleteMessageLabel)
+      ).toContain(deleteEventData.expectedDeleteConfirmationMessage);
+      await basePage.clickElement(deleteEventPage.cancelEventButton);
+      await deleteEventPage.alertDialog.waitFor({ state: "hidden" });
+      expect(await deleteEventPage.alertDialog.isHidden()).toBe(true);
+    };
+
+    // Open delete popup and verify alert dialog behavior
+    await deleteEventPage.openDeletePopup();
+    await verifyAlertDialogVisibleAndHidden();
+
+    // Open delete popup again and verify alert dialog behavior
+    await deleteEventPage.openDeletePopup();
+    await verifyAlertDialogVisibleAndHidden();
+  } catch (error: any) {
+    console.error(`Test failed: ${error.message}`);
+    throw error;
+  }
+});
+
+test("TC0085 - verify that if the events is associated with packages, products and orders an error message appears.", async () => {
+  try {
+    await deleteEventPage.deleteReferredEvent();
+    expect(
+      await basePage.isElementVisible(deleteEventPage.failedToDeleteEventLabel)
+    ).toBe(true);
+
+    expect(
+      await basePage.getElementText(deleteEventPage.alertDialog)
+    ).not.toBeNull();
+
+    expect(
+      await basePage.getAllTextContents(deleteEventPage.alertDialog)
+    ).toContain(deleteEventData.failedMessage);
+  } catch (error: any) {
+    console.error(`Test failed: ${error.message}`);
+    throw error;
+  }
+});
