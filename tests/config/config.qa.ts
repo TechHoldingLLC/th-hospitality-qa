49191d8575d236a0d2dccf9d36e587283927fa83
meet-gondaliya-th <meet.gondaliya@techholding.co>
2025-01-20 16:56:36 +0530

    Update admin porta url

--- tests/config/config.qa.ts
+++ tests/config/config.qa.ts
@@ -1,5 +1,5 @@
 export const config = {
-  adminPortalUrl: "https://admin.qa.hospitality.thinfra.net/login",
+  adminPortalUrl: "https://admin.qa.cocacola.hostedseats.com/",
   email: "prabhav.joshi@techholding.co",
   password: "Test@123",
   shopPortalUrl: "https://shop.qa.cocacola.hostedseats.com/",
